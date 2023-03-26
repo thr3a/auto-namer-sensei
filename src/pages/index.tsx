c9faafe5542a7e3801e7947131baf4dbf4c546d0
thr3a <thr3a@example.com>
2023-03-26 18:33:49 +0900

    update: 移植

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
-import type { NextPage } from 'next';
-import { Anchor } from '@mantine/core';
+import type { NextPage, GetServerSideProps } from 'next';
+import { TaskForm } from '@/features/task/Form';
+import { Text } from '@mantine/core';
 
-const IndexPage: NextPage = () => {
+type Props = {
+  csrfToken: string;
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const csrfToken = res.getHeader('X-CSRF-Token') || 'missing';
+  return { props: { csrfToken } };
+};
+
+const IndexPage: NextPage<Props> = ({ csrfToken }) => {
   return (
     <>
-      <Anchor href="/page2">Google</Anchor>
+      <Text fz="sm" mb="md">タイトルと箇条書きから文章を生成します。</Text>
+      <TaskForm csrfToken={csrfToken}></TaskForm>
     </>
   );
 };
